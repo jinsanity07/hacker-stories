6abdbc076491b323795068f9fc1bf77931ac2b15
jinsanity07 <zihaojin@uwm.edu>
2022-06-25 00:48:12 -0400

    14.Callback Handlers in JSX

--- src/App.js
+++ src/App.js
@@ -17,17 +17,21 @@ const list = [
 const App = () =>{
   const stories = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
   console.log('App renders');
+  // A 
+  const handleSearch = (event) => {
+  // C
+  console.log(event.target.value); };
+
   return ( <div> 
   
             <h1> {welcome.greeting} {welcome.title} </h1>
             <h1>Hello {getTitle('React')}</h1>
 
-            <Search />
+            <Search onSearch={handleSearch} />
 
             
              <hr />
              <List list={stories} />
-            <Search/>
               </div> );
 
 }
@@ -55,7 +59,7 @@ const List = (props) => {
       );
 
 
-  const Search = () => {
+  const Search = (props) => {
     // let searchTerm = '';
     const [searchTerm, setSearchTerm] = React.useState('');
 
@@ -64,18 +68,17 @@ const List = (props) => {
       console.log(event.target.value); 
       // a function to update this state
       setSearchTerm(event.target.value);
+      // B 
+      props.onSearch(event);
     };
-
-
 
     return ( 
     <div>
 
-    <label htmlFor="search">Search: </label>
-    
-    <input id="search" type="text" onChange={handleChange}/>
+      <label htmlFor="search">Search: </label>      
+      <input id="search" type="text" onChange={handleChange}/>
+      <p> Searching for <strong>{searchTerm}</strong>.</p>
 
-    <p> Searching for <strong>{searchTerm}</strong>.</p>
     </div> );
 
 }
