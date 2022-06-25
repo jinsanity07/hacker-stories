599ccc3a73b55f57d716ae317850d2aee759f529
jinsanity07 <zihaojin@uwm.edu>
2022-06-24 22:21:15 -0400

    5.React JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-const title = 'React';
+const welcome = { greeting: 'Hey', title: 'React!', };
+function getTitle(title) { return title; }
 
-function App() { return ( <div> <h1>Hello World</h1> </div> ); }
+
+function App() {
+
+return ( <div> 
+  
+<h1> {welcome.greeting} {welcome.title} </h1>
+<h1>Hello {getTitle('React')}</h1>
+<label htmlFor="search">Search: </label>
+
+<input id="search" type="text" />
+
+</div> );
+
+}
 
 export default App;
