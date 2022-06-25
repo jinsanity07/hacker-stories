667941f85b37df445f5f6bd46cb9f65943a1ca1a
jinsanity07 <zihaojin@uwm.edu>
2022-06-24 23:31:25 -0400

    12.React Props

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const list = [
     points: 5, objectID: 1, }, ];
 
 const App = () =>{
-
+  const stories = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
   return ( <div> 
   
             <h1> {welcome.greeting} {welcome.title} </h1>
@@ -22,29 +22,36 @@ const App = () =>{
 
             <Search />
 
-            <hr />
-             <List />
+            
              <hr />
-             <List />
+             <List list={stories} />
             <Search/>
               </div> );
 
 }
 
-const List = () => {
+const List = (props) => {
 
-  return ( <ul> {list.map( (item) => {
-                 return ( <li key={item.objectID}> <span> <a href={item.url}>{item.title}</a> </span> <span>{item.author}</span> <span>{item.num_comments}</span> <span>{item.points}</span>
-          
-          </li>
-          
-          );
-          
-          })}
-          
+  return ( <ul> 
+      {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+
+      ))}
           </ul> );
   
-  }
+  };
+
+  const Item = (props) => ( 
+      <li> 
+        <span> 
+          <a href={props.item.url}>{props.item.title}</a> 
+        </span> 
+        <span>{props.item.author}</span> 
+        <span>{props.item.num_comments}</span> 
+        <span>{props.item.points}</span> 
+      </li> 
+      );
+
 
   const Search = () => { 
     const handleChange = (event) => { 
