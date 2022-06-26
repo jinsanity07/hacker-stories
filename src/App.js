6dc69fbaebdd7b5d7f7edcfe70e32c017b8aa5ce
jinsanity07 <zihaojin@uwm.edu>
2022-06-25 23:11:36 -0400

    18.React Side-Effects

--- src/App.js
+++ src/App.js
@@ -18,9 +18,22 @@ const App = () =>{
   const stories = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
   console.log('App renders');
   
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+    );
+    
+    React.useEffect(() => { 
+      localStorage.setItem('search', searchTerm); 
+    }, [searchTerm]);
 
-  const handleSearch = (event) => { setSearchTerm(event.target.value); };
+    const handleSearch = (event) => { 
+      setSearchTerm(event.target.value); 
+    };
+
+  // const handleSearch = (event) => { 
+  //       setSearchTerm(event.target.value); 
+  //       localStorage.setItem('search', event.target.value);
+  //     };
 
   const searchedStories = stories.filter((story) => 
       story.title.toLowerCase().includes(searchTerm.toLowerCase()) );
