97e2ad202acce2eed110b5b2461b05946089ea57
jinsanity07 <zihaojin@uwm.edu>
2022-06-25 21:41:38 -0400

    16.React Controlled Components

--- src/App.js
+++ src/App.js
@@ -18,19 +18,19 @@ const App = () =>{
   const stories = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
   console.log('App renders');
   
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState('React');
 
   const handleSearch = (event) => { setSearchTerm(event.target.value); };
 
   const searchedStories = stories.filter((story) => 
       story.title.toLowerCase().includes(searchTerm.toLowerCase()) );
       
-  return ( <div> 
+    return ( <div> 
   
             <h1> {welcome.greeting} {welcome.title} </h1>
             <h1>Hello {getTitle('React')}</h1>
 
-            <Search onSearch={handleSearch} />
+            <Search search={searchTerm} onSearch={handleSearch} />
 
             
              <hr />
@@ -65,7 +65,10 @@ const Search = (props) => ( <div>
 
   <label htmlFor="search">Search: </label>
   
-  <input id="search" type="text" onChange={props.onSearch} />
+  <input id="search" 
+         type="text"
+         value={props.search}
+         onChange={props.onSearch} />
   
   </div> );
 
