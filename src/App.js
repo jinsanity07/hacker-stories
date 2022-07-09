207ea14dd056934ef1138901152689d1d3229dd0
jinsanity07 <zihaojin@uwm.edu>
2022-07-09 19:57:32 -0400

    35.2 separate the component

--- src/App.js
+++ src/App.js
@@ -141,25 +141,12 @@ const App = () =>{
     return ( 
       <div>
       <h1>My Hacker Stories</h1>
-      <form onSubmit={handleSearchSubmit}>
-        <InputWithLabel
-          id="search"
-          value={searchTerm}
-          isFocused
-          onInputChange={handleSearchInput}
-        >
-          <strong>Search:</strong>
-
-        </InputWithLabel>
-
-        <button
-          type="submit"
-          disabled={!searchTerm}
-          // onClick={handleSearchSubmit}
-        >
-          Submit
-        </button>
-      </form>
+      
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
       {stories.isError && <p>Something went wrong ...</p>}
@@ -171,6 +158,27 @@ const App = () =>{
     );
 }
 
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({
   id,
