a0d0cf206e9b1bbdf8f21d02c4f41710713db797
jinsanity07 <zihaojin@uwm.edu>
2022-07-08 22:17:39 -0400

    19. React Custom Hooks (Advanced)

--- src/App.js
+++ src/App.js
@@ -14,29 +14,35 @@ const list = [
     author: 'Dan Abramov, Andrew Clark', num_comments: 2, 
     points: 5, objectID: 1, }, ];
 
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
 const App = () =>{
   const stories = [ { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
   console.log('App renders');
-  
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || 'React'
-    );
-    
-    React.useEffect(() => { 
-      localStorage.setItem('search', searchTerm); 
-    }, [searchTerm]);
 
-    const handleSearch = (event) => { 
-      setSearchTerm(event.target.value); 
-    };
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
 
-  // const handleSearch = (event) => { 
-  //       setSearchTerm(event.target.value); 
-  //       localStorage.setItem('search', event.target.value);
-  //     };
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-  const searchedStories = stories.filter((story) => 
-      story.title.toLowerCase().includes(searchTerm.toLowerCase()) );
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
       
     return ( <div> 
   
